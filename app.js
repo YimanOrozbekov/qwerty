1a9664c0444b20c86f73a2285d934fe2d9adec93
Orozbekov Yiman <127510137+YimanOrozbekov@users.noreply.github.com>
2023-05-17 18:11:10 +0600

    qwerty

---
+++ app.js
@@ -0,0 +1,45 @@
+// var array = [null, 123, undefined, null, 'qwerty', 34, 36, undefined, null, 'ewferv']
+// var typeNull = []
+// var typeStr = []
+// var typeNum = []
+// var typeUndefined = []
+//
+// for (var i = 0; i < array.length; i++){
+//     if(array[i] === null){
+//         typeNull.push(array[i])
+//     }else if(typeof array[i] === 'string'){
+//         typeStr.push(array[i])
+//     }else if(typeof array[i] === 'number'){
+//         typeNum.push(array[i])
+//     }else if(typeof array[i] === 'undefined'){
+//         typeUndefined.push(array[i])
+
+function getAllsum() {
+    let sum = 0
+    for
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
